c40d5d746fbb44c61a6cf18a7b8fed862f0d91ca
cauuabispoo <53126609+cauuabispoo@users.noreply.github.com>
2024-02-29 22:03:44 -0300

    Add files via upload

--- CalculaTotal.js
+++ CalculaTotal.js
@@ -8,22 +8,48 @@ geral.forEach(function(geral) {
 
     // Captura o valor unitário do produto
     var unitario = parseFloat(geral.querySelector(".valor").textContent);
+    geral.querySelector(".valor").textContent = formatacao(unitario);
 
     // Valida a quantidade
-    if (qtde < 1 || isNaN(qtde)) {
+    if (validaQtde(qtde) == false) {
         // Quantidade NOK, avise o usuário
         geral.querySelector(".qtd").textContent = "QTDE INVÁLIDA!";
         geral.querySelector(".qtd").style.color = "red";
-    } else if (unitario < 1 || isNaN(unitario)) {
+        // Valida o valor
+    } else if (validaValor(unitario) == false) {
         // Valor NOK, avise o usuário
         geral.querySelector(".valor").textContent = "VALOR INVÁLIDO!";
         geral.style.backgroundColor = "red";
     } else {
         // Quantidade OK, pode prosseguir
         // Calcula o valor total da encomenda
-        geral.querySelector(".total").textContent = calculaTotal(qtde, unitario);
+        geral.querySelector(".total").textContent = formatacao(calculaTotal(qtde, unitario));
     }
 });
+
+// Valida a Quantidade
+function validaQtde(qtde){
+    valida = true;
+    if(qtde < 1 || isNaN(qtde)){
+        valida = false;
+    } 
+    return valida
+}
+
+// Valida o Valor
+function validaValor(valor){
+    valida = true;
+    if(valor < 1 || isNaN(valor)){
+        valida = false;
+    } 
+    return valida
+}
+
+// Fomata os valores para Real e converte para Float
+function formatacao(valor){
+    parseFloat(valor);
+    return valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'});
+}
 
 // Função para calcular o valor total da encomenda
 function calculaTotal(qtde, unitario) {
